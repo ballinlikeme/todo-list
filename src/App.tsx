ade92fc58d94bc74142f7e3ea7b13de3a82fd10d
Maxim Spirkin <maxim.spirkin2003@gmail.com>
2022-07-13 01:18:39 +0300

    added todos displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AddTodo } from "./components/add-todo/AddTodo";
 import { TodoList } from "./components/todo-list/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState<Array<object>>([]);
   return (
     <div className="bg-zinc-900 h-screen py-10">
       <div className="container mx-auto text-white font-mono">
@@ -10,7 +11,7 @@ function App() {
         <div>
           <AddTodo />
         </div>
-        <TodoList />
+        <TodoList todos={todos} />
       </div>
     </div>
   );
--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { TodoItem } from "../todo-item/TodoItem";
 
-export const TodoList: React.FC = () => {
-  return <ul></ul>;
+type TodoListProps = {
+  todos: Array<object>;
+};
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return <TodoItem />;
+      })}
+    </ul>
+  );
 };
